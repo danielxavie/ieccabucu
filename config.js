64f56777c9a0d8d984116287a2f50749938be540
danielxavie <danielxl.ifrj@gmail.com>
2026-08-30 15:38:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const EVENT_CONFIG = {
 
   // Substitua pelo endereço real da igreja.
   address: "Endereço da igreja, Estrada Antônio Cícero de Souza, n° 305 - Cabuçu - Itaboraí - RJ",
-  mapsUrl: "https://maps.app.goo.gl/TCbEH7TFTc8B7pDu6?g_st=ic",
+  mapsUrl: "https://maps.app.goo.gl/yB3LV7Ee9A5eSXEk9",
 
   social: {
     instagram: "https://www.instagram.com/ieccabucu?igsi=aHA2eGU4bGdpNGRs&utm_source=qr",
